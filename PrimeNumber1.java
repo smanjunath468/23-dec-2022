cc8900be8d398bf8989627085d75964dcdc61fea
smanjunath468 <116783600+smanjunath468@users.noreply.github.com>
2022-12-23 13:03:24 +0530

    22/12/2022

---
+++ PrimeNumber1.java
@@ -0,0 +1,39 @@
+package books;
+import java.util.*;
+public class PrimeNumber1 {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to checak prime or not : ");
+		int j = sc.nextInt();
+		boolean a = true;
+		
+		
+		
+		for(int i=2; i<j; i++) {
+			if(j%i==0) {
+				a = false;
+			}
+		
+		}
+
+	
+		if(a ==false) {
+			System.out.println("Its not prime");
+		}
+		else if(a == true){
+			System.out.println("Its  prime");	
+		}
+		else {
+			System.out.println("invalid");
+		}
+		
+
+	}
+
+	
+}
